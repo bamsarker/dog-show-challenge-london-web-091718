0957d15b1fda52e641cf0271b5f8a15df5bc9bf2
Sam Barker <samuel.e.barker@googlemail.com>
2018-11-10 15:01:08 +0000

    added selectedDog to state, no longer storing id in an attribute on an HTML element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const tableBody = document.querySelector('#table-body')
   const dogForm = document.querySelector('#dog-form')
@@ -7,32 +5,51 @@ document.addEventListener('DOMContentLoaded', () => {
   const breedInput = document.querySelector('input[type="breed"]')
   const sexInput = document.querySelector('input[type="sex"]')
 
-
   const state = {
-    dogs: []
+    dogs: [],
+    selectedDog: undefined
   }
 
   const renderDog = dog => {
     const tableRowItem = document.createElement('tr')
     tableRowItem.classList.add('table-row')
     tableRowItem.dataset.dogId = dog.id
-    tableRowItem.innerHTML =
-    `<td>${dog.name}</td>
+    tableRowItem.innerHTML = `<td>${dog.name}</td>
     <td>${dog.breed}</td>
     <td>${dog.sex}</td>
     <td><button class="edit-btn" data-dog-id ="${dog.id}">Edit Dog</button></td>
-    <td><button class="remove-btn" data-dog-id ="${dog.id}">Delete Dog</button></td>
+    <td><button class="remove-btn" data-dog-id ="${
+      dog.id
+    }">Delete Dog</button></td>
     `
+
     tableBody.appendChild(tableRowItem)
+
+    tableRowItem.querySelector('.edit-btn').addEventListener('click', () => {
+      state.selectedDog = dog
+      prefillFormValues(dog)
+    })
+
+    tableRowItem.querySelector('.remove-btn').addEventListener('click', () => {
+      if (confirm(`Are you sure you want to delete ${dog.name}?`)) {
+        const tableRowItem = document.querySelector(
+          `tr[data-dog-id='${dog.id}']`
+        )
+        tableRowItem.remove()
+        deleteDog(dog)
+      }
+    })
+  }
+
+  const prefillFormValues = dog => {
+    nameInput.value = dog.name
+    breedInput.value = dog.breed
+    sexInput.value = dog.sex
   }
 
   const renderDogs = dogs => {
     dogs.forEach(dog => renderDog(dog))
   }
-
-  const findDog = id =>
-    state.dogs.find(dog => dog.id === parseInt(id))
-
 
   getDogs()
     .then(dogs => {
@@ -40,67 +57,22 @@ document.addEventListener('DOMContentLoaded', () => {
       renderDogs(state.dogs)
     })
 
-
-  document.addEventListener('click', event => {
-    if(event.target.className === 'edit-btn') {
-      const id = event.target.dataset.dogId
-      const foundDog = findDog(id)
-
-    const dogInfo = {
-      name: foundDog.name,
-      breed: foundDog.breed,
-      sex: foundDog.sex
-    }
-
-    // dogForm.setAttribute('data-dog-id', `${id}`)
-    nameInput.setAttribute('value', `${dogInfo.name}`)
-    breedInput.setAttribute('value', `${dogInfo.breed}`)
-    sexInput.setAttribute('value', `${dogInfo.sex}`)
-    nameInput.setAttribute('data-dog-id', `${id}`)
-    }
-
-    if(event.target.className === 'remove-btn') {
-      const deleteDogId = event.target.dataset.dogId
-      const foundDogToDelete = findDog(deleteDogId)
-      const tableRowItem = document.querySelector(`tr[data-dog-id='${deleteDogId}']`)
-      if(confirm(`Are you sure you want to delete ${foundDogToDelete.name}?`)) {
-        tableRowItem.remove()
-        deleteDog(foundDogToDelete)
-      } else {
-        return ''
-      }
-    }
-  })
-
-
   dogForm.addEventListener('submit', event => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const id = nameInput.dataset.dogId
-    const foundDog = findDog(id)
+    state.selectedDog.name = nameInput.value
+    state.selectedDog.breed = breedInput.value
+    state.selectedDog.sex = sexInput.value
 
-    const updateDogInfo = {
-      id: foundDog.id,
-      name: nameInput.value,
-      breed: breedInput.value,
-      sex: sexInput.value
-    }
-
-    updateDog(updateDogInfo);
-
-    nameInput.setAttribute('value', `${updateDogInfo.name}`)
-    breedInput.setAttribute('value', `${updateDogInfo.breed}`)
-    sexInput.setAttribute('value', `${updateDogInfo.sex}`)
-
-    dogForm.reset();
-
-    getDogs()
+    updateDog(state.selectedDog)
+      .then(getDogs)
       .then(dogs => {
-        state.dogs = [...dogs];
-        tableBody.innerHTML = '';
-        renderDogs(state.dogs);
+        state.dogs = [...dogs]
+        tableBody.innerHTML = ''
+        renderDogs(state.dogs)
       })
 
+    state.selectedDog = undefined
+    dogForm.reset()
   })
-
 })
